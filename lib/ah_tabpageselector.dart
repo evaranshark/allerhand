4f4d6353d42ec2fe26072ec7589184195d88a3c1
evaranshark <evarancorp@gmail.com>
2023-02-02 12:37:41 +0300

    Lighten up tab page selector

--- lib/ah_tabpageselector.dart
+++ lib/ah_tabpageselector.dart
@@ -1,6 +1,3 @@
-import 'dart:ui';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 /// Uses [CustomTabPageSelectorIndicator] to display a row of small circular
@@ -25,7 +22,7 @@ class CustomTabPageSelector extends StatelessWidget {
       this.borderWidth})
       : assert(indicatorSize != null && indicatorSize > 0.0);
 
-  /// This widget's selection and animation state.
+  /// This widget's selection.
   ///
   /// If [TabController] is not provided, then the value of
   /// [DefaultTabController.of] will be used.
@@ -55,37 +52,14 @@ class CustomTabPageSelector extends StatelessWidget {
   Widget _buildTabIndicator(
     int tabIndex,
     TabController tabController,
-    ColorTween selectedColorTween,
-    ColorTween previousColorTween,
+    Color selectedColor,
+    Color backgroundColor,
+    Color borderColor,
   ) {
-    final Color background;
-    if (tabController.indexIsChanging) {
-      // The selection's animation is animating from previousValue to value.
-      final double t = 1.0 - _indexChangeProgress(tabController);
-      if (tabController.index == tabIndex) {
-        background = selectedColorTween.lerp(t)!;
-      } else if (tabController.previousIndex == tabIndex) {
-        background = previousColorTween.lerp(t)!;
-      } else {
-        background = selectedColorTween.begin!;
-      }
-    } else {
-      // The selection's offset reflects how far the TabBarView has / been dragged
-      // to the previous page (-1.0 to 0.0) or the next page (0.0 to 1.0).
-      final double offset = tabController.offset;
-      if (tabController.index == tabIndex) {
-        background = selectedColorTween.lerp(1.0 - offset.abs())!;
-      } else if (tabController.index == tabIndex - 1 && offset > 0.0) {
-        background = selectedColorTween.lerp(offset)!;
-      } else if (tabController.index == tabIndex + 1 && offset < 0.0) {
-        background = selectedColorTween.lerp(-offset)!;
-      } else {
-        background = selectedColorTween.begin!;
-      }
-    }
     return CustomTabPageSelectorIndicator(
-      backgroundColor: background,
-      borderColor: selectedColorTween.end!,
+      backgroundColor:
+          (tabController.index == tabIndex) ? selectedColor : backgroundColor,
+      borderColor: borderColor,
       size: indicatorSize,
       borderStyle: borderStyle ?? BorderStyle.solid,
       borderWidth: borderWidth,
@@ -97,10 +71,6 @@ class CustomTabPageSelector extends StatelessWidget {
     final Color fixColor = color ?? Colors.transparent;
     final Color fixSelectedColor =
         selectedColor ?? Theme.of(context).colorScheme.secondary;
-    final ColorTween selectedColorTween =
-        ColorTween(begin: fixColor, end: fixSelectedColor);
-    final ColorTween previousColorTween =
-        ColorTween(begin: fixSelectedColor, end: fixColor);
     final TabController? tabController =
         controller ?? DefaultTabController.maybeOf(context);
     assert(() {
@@ -115,43 +85,17 @@ class CustomTabPageSelector extends StatelessWidget {
       }
       return true;
     }());
-    final Animation<double> animation = CurvedAnimation(
-      parent: tabController!.animation!,
-      curve: Curves.fastOutSlowIn,
-    );
-    return AnimatedBuilder(
-      animation: animation,
-      builder: (BuildContext context, Widget? child) {
-        return Semantics(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children:
-                List<Widget>.generate(tabController.length, (int tabIndex) {
-              return _buildTabIndicator(tabIndex, tabController,
-                  selectedColorTween, previousColorTween);
-            }).toList(),
-          ),
-        );
-      },
+    return Semantics(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: List<Widget>.generate(tabController!.length, (int tabIndex) {
+          return _buildTabIndicator(tabIndex, tabController, fixSelectedColor,
+              fixColor, fixSelectedColor);
+        }).toList(),
+      ),
     );
   }
-}
-
-double _indexChangeProgress(TabController controller) {
-  final double controllerValue = controller.animation!.value;
-  final double previousIndex = controller.previousIndex.toDouble();
-  final double currentIndex = controller.index.toDouble();
-
-  // The controller's offset is changing because the user is dragging the
-  // TabBarView's PageView to the left or right.
-  if (!controller.indexIsChanging) {
-    return clampDouble((currentIndex - controllerValue).abs(), 0.0, 1.0);
-  }
-
-  // The TabController animation's value is changing from previousIndex to currentIndex.
-  return (controllerValue - currentIndex).abs() /
-      (currentIndex - previousIndex).abs();
 }
 
 /// Displays a single circle with the specified size, border style, border color
@@ -187,7 +131,7 @@ class CustomTabPageSelectorIndicator extends StatelessWidget {
   /// Defaults to [BorderStyle.solid] if value is not specified.
   final BorderStyle borderStyle;
 
-  final borderWidth;
+  final double? borderWidth;
 
   @override
   Widget build(BuildContext context) {
@@ -198,7 +142,7 @@ class CustomTabPageSelectorIndicator extends StatelessWidget {
       decoration: BoxDecoration(
         color: backgroundColor,
         border: Border.all(
-            color: borderColor, style: borderStyle, width: borderWidth),
+            color: borderColor, style: borderStyle, width: borderWidth!),
         shape: BoxShape.circle,
       ),
     );
