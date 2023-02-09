ea9f1f140209b827e2c78c8091e70a44e63639ef
evaranshark <evarancorp@gmail.com>
2023-02-09 23:24:43 +0300

    More detalised page skeleton

--- lib/pages/interview_page.dart
+++ lib/pages/interview_page.dart
@@ -1,6 +1,7 @@
 import 'package:allerhand_test/utils/locator.dart';
 import 'package:allerhand_test/utils/my_router_delegate.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -12,17 +13,18 @@ class InterviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      resizeToAvoidBottomInset: false,
+      //resizeToAvoidBottomInset: false,
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(178, 70, 320, 70),
+        padding: const EdgeInsets.fromLTRB(178, 70, 320, 60),
         child: Column(
+          mainAxisSize: MainAxisSize.max,
           children: [
             CustomAppBar(
               height: 120,
               title: 'Фамилия Имя Отчество',
               onBackPressed: _onBackPressed,
             ),
-            Expanded(child: Placeholder()),
+            Expanded(child: InterviewPageContent()),
           ],
         ),
       ),
@@ -30,4 +32,39 @@ class InterviewPage extends StatelessWidget {
   }
 
   _onBackPressed() {}
+}
+
+class InterviewPageContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 160),
+      child: LayoutBuilder(builder: (context, constraints) {
+        return Align(
+          alignment: Alignment.topLeft,
+          child: SizedBox(
+            width: constraints.maxWidth * .67,
+            height: constraints.maxHeight,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Placeholder(
+                  fallbackHeight: 82,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 36),
+                    child: Placeholder(),
+                  ),
+                ),
+                Placeholder(
+                  fallbackHeight: 205,
+                )
+              ],
+            ),
+          ),
+        );
+      }),
+    );
+  }
 }
